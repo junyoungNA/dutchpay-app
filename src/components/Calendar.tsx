004f0106639e1f2eff837f15efb7ff978025c0e3
junyoungNA <115803423+junyoungNA@users.noreply.github.com>
2023-09-11 18:23:33 +0900

    캘린더 스타일링 중

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,18 +1,49 @@
 import {  useEffect, useState } from 'react';
-import { getDate,getFullYear, getMonth, getDay } from '../util/getCurrentDate';
+import { getMonth } from '../util/getCurrentDate';
 import changeDate from '../util/changeDate';
+import OverlayWrapper from './shared/OverlayWrapper';
+import { Col, Row, Table } from 'react-bootstrap';
+import styled from 'styled-components';
 
 const Calendar = () => {
-    const [totalDate , setTotalDate] = useState();
+    const date = ['일','월','화','수','목','금','토',];
+    const [totalDate , setTotalDate] = useState<number[]>([]);
     useEffect(() => {
-        console.log(changeDate(getMonth()));
-        // setTotalDate(changeDate()));
+        setTotalDate(changeDate(getMonth()));
         }, []);
     return (
-        <div>
-            캘린더 입니다.
-        </div>
+        <OverlayWrapper minheight={'73vh'} padding='100px'>
+            <StyledCalendarRow>
+                {date.map((day) => 
+                    { 
+                        switch(day) {
+                            case '일' : 
+                            return (<StyledCalendarCol key={day} color='#b61233'>{day}</StyledCalendarCol>);
+                            case '토' :
+                            return (<StyledCalendarCol key={day} color='#0a6ba3'>{day}</StyledCalendarCol>);
+                            default :
+                            return (<StyledCalendarCol key={day}>{day}</StyledCalendarCol>);
+                        }
+                        
+                    }
+                )}
+            </StyledCalendarRow>
+        </OverlayWrapper>
     )
 }
 
 export default Calendar
+
+interface StyledCalendarColProps {
+    color? : string;
+}
+
+const StyledCalendarRow = styled(Row)`
+    font-size: 20px;
+    font-weight: 700;
+`
+
+const StyledCalendarCol = styled(Col)<StyledCalendarColProps>`
+    text-align: center;
+    color :  ${({color}) => (color ? color : 'black')};
+`
