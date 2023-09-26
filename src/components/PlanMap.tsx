7bca1c90a79c703d234442ec5cb90bcdbff1521c
junyoungNA <115803423+junyoungNA@users.noreply.github.com>
2023-09-27 02:53:13 +0900

    장소 검색 탭에서 카테고리 검색 탭 이동 시 기존 카테고리 검색결과로 마커생성

--- src/components/PlanMap.tsx
+++ src/components/PlanMap.tsx
@@ -7,6 +7,8 @@ import { useRecoilState } from 'recoil';
 import { IKakaoAddressInfo, kakaoAddressInfoState } from '../state/kakaoAddressInfo';
 import { Col, Row, Button, Tabs, Tab, Form,ListGroup, Card, CloseButton, Badge  } from 'react-bootstrap';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import { isAnyArrayBuffer } from 'util/types';
 
 type TMarkers  = {
     lat: string,
@@ -17,30 +19,26 @@ interface IMarkers {
     position? : TMarkers;
 }
 
-interface StyledPlaceNameProps {
-    isDeparture?: boolean;
-    isArrive?: boolean;
-}
-
 const PlanMap = () => {
+    const navigate = useNavigate();
     const [{x, y, address_name, region_2depth_name, place_name, road_address_name}, setAddressInfo] = useRecoilState(kakaoAddressInfoState);
     const [searchList, setSearchList] = useState<IKakaoAddressInfo[] >([]);
     const [zoomable, setZoomable] = useState(true) //zoom 막기
     const [markers, setMarkers] = useState<IMarkers[]>([]);
     const [map, setMap] = useState<any>();
-    const [markerInfo, setMarkerInfo] = useState<any>({});
+    const [markerInfo, setMarkerInfo] = useState<any>(null);
     const [departure, setDeparture] = useState(''); // 출발지
     const [arrive, setArrive] = useState(''); //도착지
     const [keyword, setKeyword] = useState('서울역');
 
     useEffect(() => {
         if (!map) return
-        kakaoKewordSerach();
-    }, [map])
+        if(!markerInfo) kakaoKewordSerach();
+    }, [map, markerInfo])
     
 
     const kakaoKewordSerach = () => {
-        const ps = new kakao.maps.services.Places()
+        const ps = new kakao.maps.services.Places();
         ps.keywordSearch(keyword, (data:any, status, _pagination) => {
             if (status === kakao.maps.services.Status.OK) {
                 // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
@@ -49,7 +47,6 @@ const PlanMap = () => {
                 const markers : any= [];
                 // console.log(data, '가져온 데이터');
                 for (let i = 0; i < data.length; i++) {
-                // @ts-ignore
                     markers.push({
                         ...data[i],
                         position: {
@@ -57,7 +54,6 @@ const PlanMap = () => {
                             lng: data[i].x,
                         }
                     })
-                // @ts-ignore
                 // extend 메소드: 이 메소드는 bounds 객체의 경계를 확장(extend)하는 데 사용됩니다
                     bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x))
                 }
@@ -70,8 +66,8 @@ const PlanMap = () => {
     }
     
 
-
     const handleComplete = async (data: any) => {
+        console.log(data,'data가머냐');
         const searchTxt = data.address; // 검색한 주소
         const config = { headers: {Authorization : `KakaoAK ${process.env.REACT_APP_KAKAOREST_KEY}`}}; // 헤더 설정
         const url = 'https://dapi.kakao.com/v2/local/search/address.json?query='+searchTxt; // REST API url에 data.address값 전송
@@ -82,7 +78,8 @@ const PlanMap = () => {
                     console.log(result.data,'검색어 입력');
                     const markers : any= [];
                     const addressInfo = {
-                        address_name: result.data.documents[0].address.address_name,
+                        place_name: result.data.documents[0].address.address_name,
+                        // address_name: result.data.documents[0].address.address_name,
                         road_address_name :result.data.documents[0].road_address.address_name,
                         region_2depth_name: result.data.documents[0].address.region_2depth_name,
                         y: Number(result.data.documents[0].y),//위도
@@ -92,11 +89,11 @@ const PlanMap = () => {
                         markers.push(addressInfo)
                         setMarkers(markers);
                         setAddressInfo(addressInfo);
-                        setSearchList((prev) => [...prev, addressInfo]);
-                        // setShowPostcode(fxalse);
                     }
                 }
-        })
+        }).catch((error : any) => {
+            console.log(error)
+        });
     };
 
     const onClickSerachRecord = (addressInfo : IKakaoAddressInfo)  => () => {
@@ -106,24 +103,42 @@ const PlanMap = () => {
 
     const onClickChangePoint = (placeName: string, type: "departure" | "arrive") => () => {
         // state 가 arrive면 setDeparture를 해야하고 departure면 setArrive
-            if (type === "departure") {
-                setDeparture(placeName);
-                if (arrive === placeName) {
-                    setArrive('');
-                }
-            } 
-
-            if (type === "arrive") {
-                setArrive(placeName);
-                if (departure === placeName) {
-                    setDeparture('');
-                }
+        if (type === "departure") {
+            setDeparture(placeName);
+            if (arrive === placeName) {
+                setArrive('');
             }
+        } 
+
+        if (type === "arrive") {
+            setArrive(placeName);
+            if (departure === placeName) {
+                setDeparture('');
+            }
+        }
     }
+
+    const handleTabSelect = (eventKey: string | null) => {
+        //카테고리 선택시 실행될 함수 
+        //location 에서 category이동시에 이전에 검색됐던 내용들을 다시 마커설정
+        if(eventKey === 'category' && searchList.length > 0) {
+            console.log(searchList,'기존마커들');
+            const markers : any[] = searchList.map( (item : any) =>  ({
+                ...item,
+                position: {
+                    lat: item.y,
+                    lng: item.x,
+                }
+            }));
+            setMarkers(markers);
+        }
+    }
+
     return (
         <OverlayWrapper>
             <StyledPlanRow padding={'auto'}>
-                <StyledPlanCol xs={12} md={6}>
+                {/* 맵부분 */}
+                <StyledPlanCol xs={12} sm={12} md={6}>
                     <StyledPlanMap 
                         center={{ lat: y, lng: x }}   // 지도의 중심 좌표
                         level={3}// 지도 확대 레벨
@@ -161,58 +176,60 @@ const PlanMap = () => {
                         ))}
                         <Button onClick={() => setZoomable(false)}>지도 확대/축소 끄기</Button>{" "}
                         <Button onClick={() => setZoomable(true)}>지도 확대/축소 켜기</Button>
+                        <Button onClick={() => window.location.href = (`https://map.kakao.com/?sName=${departure}&eName=${arrive}`)}>길찾기</Button>
                     </StyledPlanMap>
                     {departure && <div> 출발지 : {departure}</div>}
                     {arrive && <div> 도착지 : {arrive}</div>}
                 </StyledPlanCol>
-                <StyledPlanCol xs={12} md={4}>
-                <Tabs
-                    defaultActiveKey="category"
-                    id="uncontrolled-tab-example"
-                    className="mb-3"
-                    >
-                    <Tab eventKey="category" title="키워드 검색">
-                        <Form.Label htmlFor="category_text">키워드 검색</Form.Label>
-                        <Form.Control
-                            type="text"
-                            id="category_text"
-                            aria-describedby="passwordHelpBlock"
-                            onChange={(e) => setKeyword(e.target.value)}
-                            onKeyDown={(e) => {
-                                if (e.key === 'Enter') {
-                                    kakaoKewordSerach();
-                                }
-                            }}
-                        />
-                        <Form.Text id="passwordHelpBlock" muted>
-                            키워드 검색 예시("서울역 맛집", "인천 산책하기 좋은 곳")
-                        </Form.Text>
-                        <ListGroup as='ol' numbered>
-                            {searchList?.map((addressInfo: any) => 
-                                <StyledSearchListItem action key={addressInfo.id} onClick={onClickSerachRecord(addressInfo)}>
-                                    {departure === addressInfo?.place_name && <StyledCurrentPlaceDiv background='#198754'>{addressInfo.place_name}</StyledCurrentPlaceDiv> } 
-                                    {arrive === addressInfo?.place_name && <StyledCurrentPlaceDiv background='#dc3545'>{addressInfo.place_name}</StyledCurrentPlaceDiv>} 
-                                    {arrive !== addressInfo?.place_name && departure !== addressInfo?.place_name && <StyledCurrentPlaceDiv>{addressInfo.place_name}</StyledCurrentPlaceDiv>}
-                                    <StyledeBtnWrapper>
-                                        { departure === addressInfo?.place_name && <StyledDirectionBtn variant='success' disabled>현재 출발지</StyledDirectionBtn>}
-                                        { arrive === addressInfo?.place_name && <StyledDirectionBtn variant='danger' disabled>현재 도착지</StyledDirectionBtn>}
-                                        {departure !== addressInfo?.place_name && <StyledDirectionBtn variant="secondary"    
-                                            onClick={onClickChangePoint(addressInfo?.place_name, 'departure')}>
-                                                출발지
-                                        </StyledDirectionBtn>}
-                                        { arrive !== addressInfo?.place_name && <StyledDirectionBtn variant="danger"    
-                                            onClick={onClickChangePoint(addressInfo?.place_name, 'arrive')}
-                                            >    
-                                                도착지
+                {/* 검색 탭 부분 */}
+                <StyledPlanCol xs={12} sm={12} md={4} >
+                    <Tabs
+                        defaultActiveKey="category"
+                        onSelect={handleTabSelect} 
+                        className="mb-3"
+                        >
+                        <Tab eventKey="category" title="키워드 검색">
+                            <Form.Label htmlFor="category_text">키워드 검색</Form.Label>
+                            <Form.Control
+                                type="text"
+                                id="category_text"
+                                aria-describedby="passwordHelpBlock"
+                                onChange={(e) => setKeyword(e.target.value)}
+                                onKeyDown={(e) => {
+                                    if (e.key === 'Enter') {
+                                        kakaoKewordSerach();
+                                    }
+                                }}
+                            />
+                            <Form.Text id="passwordHelpBlock" muted>
+                                키워드 검색 예시("서울역 맛집", "인천 산책하기 좋은 곳")
+                            </Form.Text>
+                            <ListGroup as='ol' numbered>
+                                {searchList?.map((addressInfo: any) => 
+                                    <StyledSearchListItem action key={addressInfo.id} onClick={onClickSerachRecord(addressInfo)}>
+                                        {departure === addressInfo?.place_name && <StyledCurrentPlaceDiv background='#198754'>{addressInfo.place_name}</StyledCurrentPlaceDiv> } 
+                                        {arrive === addressInfo?.place_name && <StyledCurrentPlaceDiv background='#dc3545'>{addressInfo.place_name}</StyledCurrentPlaceDiv>} 
+                                        {arrive !== addressInfo?.place_name && departure !== addressInfo?.place_name && <StyledCurrentPlaceDiv>{addressInfo.place_name}</StyledCurrentPlaceDiv>}
+                                        <StyledeBtnWrapper>
+                                            { departure === addressInfo?.place_name && <StyledDirectionBtn variant='success' disabled>현재 출발지</StyledDirectionBtn>}
+                                            { arrive === addressInfo?.place_name && <StyledDirectionBtn variant='danger' disabled>현재 도착지</StyledDirectionBtn>}
+                                            {departure !== addressInfo?.place_name && <StyledDirectionBtn variant="secondary"    
+                                                onClick={onClickChangePoint(addressInfo?.place_name, 'departure')}>
+                                                    출발지
                                             </StyledDirectionBtn>}
-                                    </StyledeBtnWrapper>
-                                </StyledSearchListItem>
-                            )}
-                        </ListGroup>                    
-                    </Tab>
-                    <Tab eventKey="location" title="장소 검색">
-                        <DaumPostcode onComplete={handleComplete} autoClose={false}  style={{height:'350px'}}/>
-                    </Tab>
+                                            { arrive !== addressInfo?.place_name && <StyledDirectionBtn variant="danger"    
+                                                onClick={onClickChangePoint(addressInfo?.place_name, 'arrive')}
+                                                >    
+                                                    도착지
+                                            </StyledDirectionBtn>}
+                                        </StyledeBtnWrapper>
+                                    </StyledSearchListItem>
+                                )}
+                            </ListGroup>                    
+                        </Tab>
+                        <Tab eventKey="location" title="장소 검색">
+                            <DaumPostcode onComplete={handleComplete} autoClose={false}  style={{height:'500px'}}/>
+                        </Tab>
                     </Tabs>
                 </StyledPlanCol>
             </StyledPlanRow>
@@ -238,6 +255,8 @@ const StyledPlanCol = styled(Col)`
 const StyledMapCard = styled(Card)`
     position: relative;
     padding: 10px 20px;
+    z-index: 1;
+
     /* 다른 공통 스타일 속성 추가 가능 */
     & > .card-body {
         height: inherit;
