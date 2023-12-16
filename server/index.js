6edab4757ea8712da88edf7512202f5fd6d4fc77
junyoungNA <115803423+junyoungNA@users.noreply.github.com>
2023-12-17 04:05:36 +0900

    ec2 server port 80으로 변경

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 4000;
+const port = 80;
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
